e8a9ae7899f95b723fc35848060afddea61673f2
Cletous <ngomacletousjnr@gmail.com>
2026-03-08 13:04:06 +0200

    Update export-project-structure.php

--- config/export-project-structure.php
+++ config/export-project-structure.php
@@ -14,6 +14,7 @@ return [
         'requests' => 'app/Http/Requests',
         'jobs' => 'app/Jobs',
         'policies' => 'app/Policies',
+        'middleware' => 'app/Http/Middleware',
         'seeders' => 'database/seeders',
         'pages' => 'resources/js/Pages',
         'hooks' => 'resources/js/hooks',
